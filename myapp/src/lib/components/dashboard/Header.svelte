012187309bb6026e11f49ea5681dec9c1741c10e
Justin Fleagle <19858424+SwimSwag10@users.noreply.github.com>
2023-04-13 15:53:04 -0400

    bugfix-02

--- myapp/src/lib/components/dashboard/Header.svelte
+++ myapp/src/lib/components/dashboard/Header.svelte
@@ -1,14 +1,13 @@
 <script>
-  import { logo } from "$lib/assets/user.svg"
-  // import {clickOutside} from '$lib/utils/clickOutside.js'
+  // import { logo } from "$lib/assets/user.svg"
+  import { clickOutside } from '$lib/utils/clickOutside.js'
 
-  // let showDropdown = false;
-  // let showSettings = false;
+  let showDropdown = false;
+  let showSettings = false;
 </script>
 
-<div class="fixed w-screen flex gap-4 items-center h-[5rem] top-0 z-[1] bg-white border-b">
-  <h2>hey</h2>
-  <!-- <img src={logo} alt class="m-2 w-[16.875rem]" draggable="false">
+<div class="fixed w-screen flex gap-4 items-center h-20 top-0 z-[1] bg-white border-b">
+  <!-- <img src="" alt class="m-2 w-[16.875rem]" draggable="false"> -->
   <span class="font-bold text-lg border px-1 text-blue-600 bg-blue-200 border-blue-200 border-4 rounded-md select-none">BETA</span>
   <div class="flex px-5 basis-full items-center">
     <div class="flex ml-auto gap-4 items-center select-none">
@@ -17,15 +16,11 @@
         {#if showDropdown}
           <div class="absolute flex flex-col top-[110%] bg-sky-50 right-0 shadow rounded-md whitespace-nowrap">
             <div class="px-8 py-2">
-              <div>
-                <h2>hey</h2>
-              </div>
             </div>
             <button on:click={() => {document.body.style.overflow = "hidden"; showSettings = true; showDropdown = false;}} class="border-x border-t rounded-t-md px-8 py-2 hover:bg-sky-100">Settings</button>
-            <button class="rounded-b-md border px-8 py-2 hover:bg-sky-100">Log out</button>
           </div>
         {/if}
       </div>
     </div>
-  </div> -->
+  </div>
 </div>
---
+++ myapp/src/routes/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Header from "$lib/components/dashboard/Header.svelte";
+</script>
+
+<Header />
+
+<div>
+  <h3>hello from dashboard route page.svelte</h3>
+</div>
